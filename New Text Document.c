0628760ca830fd77bb07b161f2c8b5dcbd302e2f
manglam01 <manglamknp@gmail.com>
2017-10-31 20:38:50 +0530

    Update New Text Document.c

--- New Text Document.c
+++ New Text Document.c
@@ -1,9 +1,22 @@
-
 #include<stdio.h>
-void main()
- {int i;
-	for(i=1;i<=10;i++)
+
+int main()
 	{
-		printf("i\n");		
-	}
- }
+		int n,i,j,k;
+		scanf("%d\n",&n);
+		for(i=1;i<=n;i++)
+			{
+				for(j=1;j<=n-i;j++)
+					{
+						printf(" ");					
+					}
+				for(k=1;k<=i;k++)
+					{
+						printf("%d",k);
+					
+					}
+			printf("\n");
+			}
+	
+		return 0;
+		}
